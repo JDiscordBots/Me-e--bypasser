a2e3b2697a2e5f03873502f5bd277b4ae2ac00ef
danthe1st <daniel@wwwmaster.at>
2022-02-17 18:47:38 +0100

    OCR

--- src/main/java/io/github/jdiscordbots/mee/bypasser/MsgListener.java
+++ src/main/java/io/github/jdiscordbots/mee/bypasser/MsgListener.java
@@ -1,33 +1,33 @@
 package io.github.jdiscordbots.mee.bypasser;
 
+import io.github.jdiscordbots.mee.bypasser.model.db.GuildInformation;
+import io.github.jdiscordbots.mee.bypasser.ocr.MeeImageRecognition;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.MessageEmbed.AuthorInfo;
+import net.dv8tion.jda.api.events.ShutdownEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.sourceforge.tess4j.TesseractException;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
-
-import org.json.JSONException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.github.jdiscordbots.mee.bypasser.model.db.GuildInformation;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.MessageEmbed.AuthorInfo;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.events.ShutdownEvent;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MsgListener extends ListenerAdapter {
 
-	private static final long RATE_LIMIT = 1000L * 60;
+	private static final long RATE_LIMIT = 1000L;
 	private static final Logger LOG = LoggerFactory.getLogger(MsgListener.class);
+	private static final Pattern rankPattern = Pattern.compile("!rank (?:(?<id1>\\d+)|[<][@][!]?(?<id2>\\d+)[>])");
 
 	private DataBaseController database;
+
 
 	public MsgListener() {
 		database = DataBaseController.getInstance();
@@ -40,6 +40,7 @@ public class MsgListener extends ListenerAdapter {
 	public static class Holder<T> {
 		private T val;
 
+
 		public Holder(T val) {
 			this.val = val;
 		}
@@ -51,50 +52,79 @@ public class MsgListener extends ListenerAdapter {
 		public void set(T val) {
 			this.val = val;
 		}
+
 	}
 
 	@Override
-	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-		if (event.getMember() == null) {
+	public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+		if(!event.isFromGuild()) {
 			return;
 		}
-		if (event.getMember().getUser().isBot()) {
-			if (event.getAuthor().getIdLong() == 159985870458322944L //check if message is from Mee6
-					&& event.getGuild().getSelfMember().hasPermission(event.getChannel(), Permission.MESSAGE_HISTORY)) {
-				event.getChannel().retrieveMessageById(event.getMessageId()).queueAfter(1, TimeUnit.SECONDS,
-						msg -> runIfMemberFound(msg, member -> updateRole(member, event.getChannel(), null)));
-			}
-		} else if (event.getMessage().getContentRaw().startsWith("!rank")) { //check if message is rank query
-			final Member member;
-			if (event.getMessage().getMentionedMembers().isEmpty()) {
-				member = event.getMember();
+		if(event.getMember() == null) {
+			return;
+		}
+		if(!event.getMember().getUser().isBot()
+				|| event.getAuthor().getIdLong() != 159985870458322944L) { //check if message is from Mee6
+			return;
+		}
+		if(event.getGuild().getSelfMember().hasPermission(event.getGuildChannel(), Permission.MESSAGE_HISTORY)) {
+			Message.Interaction interaction = event.getMessage().getInteraction();
+			if(interaction == null) {
+				event.getGuildChannel().getHistory().retrievePast(2).queue(messages -> {
+					if(messages.size() == 2) {
+						Message message = messages.get(1);
+						Matcher rankPatternMatcher = rankPattern.matcher(message.getContentRaw());
+						if("!rank".equals(message.getContentRaw())) {
+							if(message.getMember() == null) {
+								event.getGuild().retrieveMemberById(message.getAuthor().getId()).queue(actualMember -> {
+									updateRole(actualMember, event, actualMember);
+								});
+							} else {
+								updateRole(message.getMember(), event, message.getMember());
+							}
+						} else if(rankPatternMatcher.matches()) {
+							String id = rankPatternMatcher.group("id1");
+							if(id == null) {
+								id = rankPatternMatcher.group("id2");
+							}
+							event.getGuild().retrieveMemberById(id).queue(member -> updateRole(member, event, message.getMember()));
+						}
+					}
+				});
 			} else {
-				member = event.getMessage().getMentionedMembers().get(0);
+				if("rank".equals(interaction.getName())) {
+					Member member = interaction.getMember();
+					if(member != null) {
+						event.getChannel().retrieveMessageById(event.getMessageId()).queueAfter(1, TimeUnit.SECONDS,
+								msg -> updateRole(member, event, msg, member));
+					}
+				}
+
 			}
-			updateRole(member, event.getChannel(), null);
 		}
 	}
 
+
 	private void runIfMemberFound(Message message, Consumer<Member> toRun) {
 		List<MessageEmbed> embeds = message.getEmbeds();
-		if (embeds.size() != 1) {
+		if(embeds.size() != 1) {
 			return;
 		}
 		MessageEmbed embed = embeds.get(0);
 		AuthorInfo author = embed.getAuthor();
-		if (author == null) {
+		if(author == null) {
 			return;
 		}
 		String iconUrl = author.getIconUrl();
-		if (iconUrl == null) {
+		if(iconUrl == null) {
 			return;
 		}
-		if (iconUrl.startsWith("https://cdn.discordapp.com/embed/avatars/")) {
+		if(iconUrl.startsWith("https://cdn.discordapp.com/embed/avatars/")) {
 			runIfMemberByPrefixFound(message.getGuild(), author, toRun);
 			return;
 		}
 		String[] splittedURL = iconUrl.split("/");
-		if (splittedURL.length != 6) {
+		if(splittedURL.length != 6) {
 			return;
 		}
 		message.getGuild().retrieveMemberById(splittedURL[4]).queue(toRun,
@@ -103,74 +133,77 @@ public class MsgListener extends ListenerAdapter {
 
 	private void runIfMemberByPrefixFound(Guild g, AuthorInfo author, Consumer<Member> toRun) {
 		g.retrieveMembersByPrefix(author.getName(), 1).onSuccess(members -> {
-			if (!members.isEmpty()) {
+			if(!members.isEmpty()) {
 				toRun.accept(members.get(0));
 			}
 		});
 	}
 
-	private void updateRole(Member member, TextChannel channel, Member author) {
-		try {
-			if (member == null) {
-				return;
-			}
-			final GuildInformation guildInfo = database.loadGuildInformation(member.getGuild().getId());
-			if (guildInfo.getLastRankCall() + RATE_LIMIT > System.currentTimeMillis()) {
-				return;
-			}
-			final int level = MeeAPI.getLevel(member.getGuild().getId(), member.getId());
-			guildInfo.setLastRankCall(System.currentTimeMillis());
-			database.save(guildInfo);
-			guildInfo.getRoleIdsAtLevel(level).forEach((roleId, add) -> {
-				final Role role = member.getGuild().getRoleById(roleId);
-				if (role != null) {
-					if (hasModPerm(member.getGuild().getSelfMember())
-							&& member.getGuild().getSelfMember().canInteract(role)) {
-						if (add.booleanValue()) {
-							if (!member.getRoles().contains(role)) {
-								member.getGuild().addRoleToMember(member, role).queue();
-							}
-						} else {
-							if (member.getRoles().contains(role)) {
-								member.getGuild().removeRoleFromMember(member, role).queue();
+	private void updateRole(Member member, MessageReceivedEvent event, Member author) {
+		updateRole(member, event, event.getMessage(), author);
+	}
+
+	private void updateRole(Member member, MessageReceivedEvent event, Message msg, Member author) {
+		final List<Message.Attachment> attachments = msg.getAttachments();
+		if(attachments.size() != 1) {
+			return;
+		}
+		final Message.Attachment attachment = attachments.get(0);
+		if(attachment.getContentType() == null || !attachment.getContentType().startsWith("image/")) {
+			return;
+		}
+		final GuildInformation guildInfo = database.loadGuildInformation(event.getGuild().getId());
+		if(guildInfo.getLastRankCall() + RATE_LIMIT > System.currentTimeMillis()) {
+			return;
+		}
+		guildInfo.setLastRankCall(System.currentTimeMillis());
+		database.save(guildInfo);
+		attachment.retrieveInputStream().thenAccept(is -> {
+			try {
+				MeeImageRecognition.LevelInfo levelInfo = MeeImageRecognition.loadDataFromImage(is);
+				if(String.valueOf(levelInfo.getDiscrimminator()).equals(member.getUser().getDiscriminator())) {
+					guildInfo.getRoleIdsAtLevel(levelInfo.getLevel()).forEach((roleId, add) -> {
+						final Role role = member.getGuild().getRoleById(roleId);
+						if(role != null) {
+							if(hasModPerm(member.getGuild().getSelfMember())
+									&& member.getGuild().getSelfMember().canInteract(role)) {
+								if(add) {
+									if(!member.getRoles().contains(role)) {
+										member.getGuild().addRoleToMember(member, role).queue();
+									}
+								} else {
+									if(member.getRoles().contains(role)) {
+										member.getGuild().removeRoleFromMember(member, role).queue();
+									}
+								}
+							} else if(author.hasPermission(Permission.ADMINISTRATOR)) {
+								event.getChannel().sendMessage("Cannot assign role " + role.getName() + " as "
+										+ member.getGuild().getSelfMember().getEffectiveName()
+										+ " does not have the necessary permissions.").queue();
 							}
 						}
-					} else if (member.hasPermission(Permission.ADMINISTRATOR)) {
-						channel.sendMessage("Cannot assign role " + role.getName() + " as "
-								+ member.getGuild().getSelfMember().getEffectiveName()
-								+ " does not have the necessary permissions.").queue();
-					}
+					});
 				}
-			});
-		} catch (JSONException e) {
-			if (LOG.isErrorEnabled()) {
-				LOG.error("Cannot load leveling data from the Mee API in guild {}", member.getGuild().getName(), e);
+			} catch(IOException e) {
+				LOG.atError()
+						.addArgument(event::getGuild)
+						.setCause(e)
+						.log("An I/O error occurred trying to read the image in guild {}");
+			} catch(TesseractException e) {
+				LOG.atInfo()
+						.addArgument(event::getGuild)
+						.setCause(e)
+						.log("An I/O error occurred trying to parse the image in guild {}");
 			}
-		} catch (IOException e) {
-			if (e.getMessage() != null
-					&& e.getMessage().startsWith("Server returned HTTP response code: 401 for URL: ")) {
-				if (author != null && author.hasPermission(Permission.ADMINISTRATOR)) {
-					channel.sendMessage(
-							"The server leaderboard needs to be public for automatic role assigning to work. This can be configured on https://mee6.xyz/dashboard/"
-									+ member.getGuild().getId() + "/leaderboard")
-							.queue();
-				}
-				if (LOG.isInfoEnabled()) {
-					LOG.info("Server leaderboard is not public for guild {}", member.getGuild().getName());
-				}
-			} else {
-				if (LOG.isErrorEnabled()) {
-					LOG.error("Cannot load leveling data from the Mee API in guild {}", member.getGuild().getName(), e);
-				}
-			}
-		}
+		});
+
 	}
 
 	@Override
 	public void onShutdown(ShutdownEvent event) {
 		try {
 			database.close();
-		} catch (Exception e) {
+		} catch(Exception e) {
 			LOG.error("cannot close database", e);
 		}
 	}
