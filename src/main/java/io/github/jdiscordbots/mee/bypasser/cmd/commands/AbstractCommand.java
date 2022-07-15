199794785523013b298c71e3986e09fcc7f0237c
danthe1st <daniel@wwwmaster.at>
2022-07-15 14:11:14 +0200

    update dependencies, fix image recognition

--- src/main/java/io/github/jdiscordbots/mee/bypasser/cmd/commands/AbstractCommand.java
+++ src/main/java/io/github/jdiscordbots/mee/bypasser/cmd/commands/AbstractCommand.java
@@ -1,14 +1,11 @@
 package io.github.jdiscordbots.mee.bypasser.cmd.commands;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.Set;
 
 import io.github.jdiscordbots.command_framework.command.CommandEvent;
 import io.github.jdiscordbots.command_framework.command.ICommand;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.interactions.commands.privileges.CommandPrivilege;
 
 public abstract class AbstractCommand implements ICommand {
 
@@ -18,16 +15,14 @@ public abstract class AbstractCommand implements ICommand {
 	}
 
 	@Override
-	public Collection<CommandPrivilege> getPrivileges(Guild guild) {
-		return guild.getRoles().stream().filter(role -> role.hasPermission(Permission.MANAGE_ROLES))
-				.map(CommandPrivilege::enable).collect(Collectors.toList());
+	public Set<Permission> getRequiredPermissions() {
+		return Collections.singleton(Permission.MANAGE_ROLES);
 	}
-
 	
 	@Override
 	public boolean allowExecute(CommandEvent event) {
-		for (CommandPrivilege priv : getPrivileges(event.getGuild())) {
-			if(event.getMember().getRoles().stream().map(Role::getId).anyMatch(priv.getId()::equals)) {
+		for(Permission perm : getRequiredPermissions()){
+			if(event.getMember().hasPermission(perm)){
 				return true;
 			}
 		}
--- src/main/java/io/github/jdiscordbots/mee/bypasser/ocr/MeeImageRecognition.java
+++ src/main/java/io/github/jdiscordbots/mee/bypasser/ocr/MeeImageRecognition.java
@@ -1,81 +1,86 @@
 package io.github.jdiscordbots.mee.bypasser.ocr;
 
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.net.URL;
 import java.nio.file.Files;
 
+import javax.imageio.ImageIO;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 public class MeeImageRecognition {
 	private static final Tesseract tesseract = new Tesseract();
-
-	static {
+	
+	static{
 		File trainingPath = new File(".tesseract");
-
+		
 		trainingPath.mkdirs();
 		File trainingDataFile = new File(trainingPath, "eng.traineddata");
-		if(!trainingDataFile.exists()) {
-			try(BufferedInputStream bis = new BufferedInputStream(new URL("https://github.com/tesseract-ocr/tessdata/blob/main/eng.traineddata?raw=true").openStream())) {
+		if(!trainingDataFile.exists()){
+			try(BufferedInputStream bis = new BufferedInputStream(new URL("https://github.com/tesseract-ocr/tessdata/blob/main/eng.traineddata?raw=true").openStream())){
 				Files.copy(bis, trainingDataFile.toPath());
-			} catch(IOException e) {
+			}catch(IOException e){
 				throw new UncheckedIOException(e);
 			}
 		}
 		tesseract.setDatapath(trainingPath.getAbsolutePath());
 		tesseract.setVariable("tessedit_char_whitelist", "RANK#LEV0123456789");
 		tesseract.setVariable("user_defined_dpi", "300");
-
+		
 	}
-
+	
 	public static LevelInfo loadDataFromImage(InputStream imageInput) throws IOException, TesseractException {
 		BufferedImage img = ImageIO.read(imageInput);
 		return new LevelInfo(loadRankTextFromImage(img), loadDiscrimminatorFromImage(img));
 	}
-
+	
 	private static int loadDiscrimminatorFromImage(BufferedImage img) throws TesseractException {
 		String seperator = "#";
-		Rectangle rect = new Rectangle(252, 122, img.getWidth() - 252 - 45, 75);
+		Rectangle rect = new Rectangle(200, 122, img.getWidth() - 200 - 45, 75);
 		return loadIntFromImage(img, seperator, rect);
 	}
-
+	
 	private static int loadRankTextFromImage(BufferedImage img) throws TesseractException {
 		String prelevel = "LEVEL";
 		Rectangle rect = new Rectangle(569, 0, img.getWidth() - 569, 118);
 		return loadIntFromImage(img, prelevel, rect);
 	}
-
+	
 	private static int loadIntFromImage(BufferedImage img, String seperator, Rectangle rect) throws TesseractException {
 		String infoText = tesseract.doOCR(img, rect);
 		int levelIndex = infoText.indexOf(seperator);
-		if(levelIndex == -1) {
+		if(levelIndex == -1){
 			return -1;
-		} else {
-			try {
+		}else{
+			try{
 				int beginIndex = levelIndex + seperator.length();
 				return Integer.parseInt(infoText.substring(beginIndex, Math.min(beginIndex + 4, infoText.length())).replace(" ", "").trim());
-			} catch(NumberFormatException e) {
+			}catch(NumberFormatException e){
 				return -1;
 			}
 		}
 	}
-
+	
 	public static class LevelInfo {
 		private final int level;
 		private final int discrimminator;
-
+		
 		public LevelInfo(int level, int discrimminator) {
 			this.level = level;
 			this.discrimminator = discrimminator;
 		}
-
+		
 		public int getLevel() {
 			return level;
 		}
-
+		
 		public int getDiscrimminator() {
 			return discrimminator;
 		}
